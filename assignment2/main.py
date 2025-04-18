67af22aa4d4547b15edfd9d4c1ea83fb194c17f8
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-04-18 16:34:08 +0300

    Initial commit

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,9 +1,12 @@
-from vehicles import Vehicle, Car, Plane
+from vehicles import Car, Plane
 while True:
     brand = input("Brand Name: ")
     model = input("Model: ")
     
     car = Car(brand, model)
+    
     plane = Plane(brand, model)
+    
     car.move()
+    
     plane.move()
